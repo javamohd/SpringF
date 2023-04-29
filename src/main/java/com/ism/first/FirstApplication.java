79c04426f5346098e2302346318584697c15e319
ISM <ISM@DESKTOP-6030GTI>
2023-04-29 20:15:16 +0530

    F Commit 01

--- src/main/java/com/ism/first/FirstApplication.java
+++ src/main/java/com/ism/first/FirstApplication.java
@@ -11,7 +11,7 @@ public class FirstApplication {
 
         @GetMapping("/")
         public String Hello(){
-            return "India is My Country.";
+            return "India is My Country. India";
         }
     
 	public static void main(String[] args) {
